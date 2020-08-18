6beb5462ede488e75bf918f34ae8e176b3a1aad5
Lehersun <kpekep@list.ru>
2020-08-18 21:04:29 +0300

    Всплывающее окно
    осталось привязать обработчики событий через делегирование

--- JS/js.js
+++ JS/js.js
@@ -36,78 +36,6 @@ $(document).ready(function () {
     }
   });
 
-  // Всплывающее окно "Заказать звонок"
-
-  $('.call__button').click(function () {
-    disableScroll();
-    $('.new-popup-background').css("display", "block");
-    $('.new-popup-window').css("display", "block")
-    $(".new-popup-window").animate({
-      opacity: 1,
-      height: "300",
-    }, 300, function () {
-      $('body').css("overflow", "hidden")
-    });
-  });
-
-  $('.new-popup-window__close-button').click(function () {
-    enableScroll();
-
-    $(".new-popup-window").animate({
-      opacity: 0.25,
-      height: "0"
-    }, 300, function () {
-      $('.new-popup-background').css("display", "none");
-      $('body').css("overflow", "visible")
-    });
-  });
-
-  // МАСКА для ввода 
-  const form_phone = $('.new-popup-window__phone');
-  const form_name = $('.new-popup-window__name');
-
-  form_phone.mask('+7(000)000-00-00');
-  form_name.bind("change keyup input click", function () {
-    if (this.value.match(/[^а-яА-Яa-zA-Z\s]/g)) {
-      this.value = this.value.replace(/[^а-яА-Яa-zA-Z\s]/g, '');
-    }
-  });
-
-
-
-  // Валидация формы
-
-  // $(".new-popup-window__form").submit(function () {
-
-  //   let phonenumber = form_phone.val().length;
-  //   event.preventDefault();
-  //   if (form_phone.val() && form_name.val()) {
-  //     if (phonenumber == 16) {
-  //       alert(
-  //         "Ваша заявка на звонок принята. Мы перезвоним Вам в ближайшее время."
-  //       );
-  //       form_phone.removeClass("error");
-  //       form_name.removeClass("error");
-
-  //       $(this).submit();
-
-  //       form_phone.val(' ');
-  //       form_name.val(' ');
-
-
-  //     } else {
-
-  //       console.log('error');
-  //     }
-  //   } else {
-
-  //     alert("Следующие поля не могут быть пустыми");
-  //     form_phone.addClass("error");
-  //     form_name.addClass("error");
-  //   }
-  // });
-
-
   // owl-carousel
 
   $(".owl-carousel").owlCarousel({
@@ -131,5 +59,126 @@ $(document).ready(function () {
       },
     },
   });
+
+  // Всплывающее окно "Заказать звонок"
+
+  function opencallwindow() {
+    disableScroll();
+    $('.new-popup-background').css("display", "block");
+    $('.new-popup-window').css("display", "block")
+    $(".new-popup-window").animate({
+      opacity: 1,
+      height: "300",
+    }, 300, function () {
+      $('body').css("overflow", "hidden")
+    });
+  }
+
+  function closecallwindow() {
+    enableScroll();
+
+    $(".new-popup-window").animate({
+      opacity: 0.25,
+      height: "0"
+    }, 300, function () {
+      $('.new-popup-background').css("display", "none");
+      $('body').css("overflow", "visible")
+    });
+  }
+
+  $('.call__button').click(opencallwindow);
+  $('.footer__call__button').click(opencallwindow);
+
+  $('.new-popup-window__close-button').click(closecallwindow);
+
+  // МАСКА для ввода 
+  const form_phone = $('.new-popup-window__phone');
+  const form_name = $('.new-popup-window__name');
+
+  form_phone.mask('+7(000)000-00-00');
+  form_name.bind("change keyup input click", function () {
+    if (this.value.match(/[^а-яА-Яa-zA-Z\s]/g)) {
+      this.value = this.value.replace(/[^а-яА-Яa-zA-Z\s]/g, '');
+    }
+  });
+
+  ///Отправка формы
+
+  var form = $(".new-popup-window__form")[0];
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    alert(
+      "Ваша заявка на звонок принята. Мы перезвоним Вам в ближайшее время"
+    );
+  }
+
+  function error() {
+    alert(
+      "К сожалению сервис сейчас не работает. Попробуйте позже"
+    );
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    const form_phone = $('.new-popup-window__phone');
+    const form_name = $('.new-popup-window__name');
+    const container = $('.speech');
+
+    const phonenumber = form_phone.val().length;
+    ev.preventDefault();
+    if (form_phone.val().trim() && form_name.val().trim()) {
+      if (phonenumber == 16) {
+        form_phone.removeClass("error");
+        form_name.removeClass("error");
+        closecallwindow();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+
+
+      } else {
+        form_phone.addClass("error");
+        $('.speech').show();
+
+      }
+    } else {
+      form_phone.addClass("error");
+      form_name.addClass("error");
+      form_phone.attr("placeholder", "Поле не может быть пустым");
+      form_name.attr("placeholder", "Поле не может быть пустым");
+
+    }
+    $(document).mouseup(function (e) {
+
+      if (container.has(e.target).length === 0) {
+        container.hide();
+        form_phone.removeClass("error");
+        form_name.removeClass("error");
+        if (form_phone.attr("placeholder") === 'Поле не может быть пустым') {
+          form_phone.attr("placeholder", "+7(123)456-78-90");
+          form_name.attr("placeholder", "Джон");
+        }
+      }
+    });
+  });
 });
-//
+//// helper function for sending an AJAX request
+
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
