898efde1f5af2bc169c051cdec5746edc020fa0e
Lehersun <kpekep@list.ru>
2020-09-02 00:47:48 +0300

    Update ajax-form.php

--- ajax-form.php
+++ ajax-form.php
@@ -12,9 +12,13 @@ $my_email = '<kpekep@list.ru>'; // Куда отправляем
 $sender_email = '<robot@lehersun.ru>'; // От кого отправляем
 $title = "Сообщение с сайта"; 
 //Сообщение, которое приходит на почту со всеми нужными нам данными:
-
+if (isset($email)) {
 $mes = " Имя: $name\n Телефон: $phone\n E-mail: $email\n Сообщение: $comment\n ";
+} 
+else {
+$mes = " Имя: $name\n Телефон: $phone\n ";
 
+}
 //Всё, теперь можно отправлять письмо на почту
 
 $send = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
