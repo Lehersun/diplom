171d934f579298f4bf104618a0d362d3e9d23e67
Lehersun <kpekep@list.ru>
2020-09-02 00:06:57 +0300

    Update ajax-form.php

--- ajax-form.php
+++ ajax-form.php
@@ -1,7 +1,6 @@
 <?php
 //Для начала проверим есть ли данные в полях name и email, что бы не слать совсем пустые формы :)
 //Если всё в порядке, то работаем дальше
-if (isset($_POST["name"]) && isset($_POST["email"]) ) { 
 //Принимаем данные POST-запроса и записываем значения в переменные
 
 $name = $_POST['name'];
@@ -10,17 +9,17 @@ $comment = $_POST['comment'];
 
 //Теперь давайте настроим куда отправляем и откуда
 
-$my_email = 'kpekep@list.ru'; // Куда отправляем
-$sender_email = '<robot@mysite.ru>'; // От кого отправляем
+$my_email = '<kpekep@list.ru>'; // Куда отправляем
+$sender_email = '<robot@lehersun.ru>'; // От кого отправляем
 $title = "Сообщение с сайта"; 
 //Сообщение, которое приходит на почту со всеми нужными нам данными:
 
-$mes = " Имя: $name\n Телефон: $phone\n E-mail: $email\n Тема обращения: $sub\n ";
+$mes = " Имя: $name\n Телефон: $phone\n E-mail: $email\n Тема обращения: $comment\n ";
 
 //Всё, теперь можно отправлять письмо на почту
 
 $send = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
 
-}
+
 
 ?>
